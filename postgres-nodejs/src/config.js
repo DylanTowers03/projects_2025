bfd6c937cbdd0f07543d62785ecfb9ebf95e0703
DylanTowers03 <ddavidtorres@uniguajira.edu.co>
2024-12-28 15:21:01 -0500

    app dockerizada

--- postgres-nodejs/src/config.js
+++ postgres-nodejs/src/config.js
@@ -1,9 +1,9 @@
 
 export const config = {
-    port: process.env.PORT,
+    port: process.env.DOCKER_PORT,
     user: process.env.PGUSER,
     host: process.env.PGHOST,
     database: process.env.PGDATABASE,
     password: process.env.PGPASSWORD,
-    dbPort: process.env.PGPORT,
+    dbPort: process.env.PG_DOCKER_PORT,
 };
